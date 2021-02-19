aaaca0fe2c960b88580b3bef9246d366b27d4244
iman20004 <imanali4@gmail.com>
2021-02-19 12:17:17 -0500

    Created collection view controller and cell

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Iman Ali on 2/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
